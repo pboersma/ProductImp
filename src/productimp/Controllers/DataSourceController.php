3edbae478e8e9997a11b3ec186f577b103dc10ab
Peter Boersma <peter@growingminds.nl>
2022-07-17 20:02:30 +0200

    Added Datasource Storage logic

--- src/productimp/Controllers/DataSourceController.php
+++ src/productimp/Controllers/DataSourceController.php
@@ -1,7 +1,8 @@
 <?php
 
-class productimp_Controllers_DataSourceController implements productimp_Controllers_Interfaces_iController  {
-    public function list(): Array
+class productimp_Controllers_DataSourceController implements productimp_Controllers_Interfaces_iController
+{
+    public function list(): array
     {
         return [];
     }
@@ -11,26 +12,41 @@ class productimp_Controllers_DataSourceController implements productimp_Controll
      */
     public function store(WP_REST_Request $request)
     {
-        $requiredFields = ['datasource_name', 'datasource_url'];
+        $requiredFields = ['datasource_name', 'datasource_url', 'datasource_credentials'];
 
-        foreach($requiredFields as $field) {
-            if(!isset($request[$field])) {
+        foreach ($requiredFields as $field) {
+            if (!isset($request[$field])) {
                 $fieldsNotFound[] = $field;
             }
         }
 
         // Early exit if required fields are missing from the request.
-        if($fieldsNotFound) {
-            return new WP_Error( 
-                'missing_fields', 
-                'Missing Required Fields', 
-                array( 'status' => 400, 'fields' => $fieldsNotFound ) 
+        if ($fieldsNotFound) {
+            return new WP_Error(
+                'missing_fields',
+                'Missing Required Fields',
+                [
+                    'status' => 400,
+                    'fields' => $fieldsNotFound
+                ]
             );
-            // return [
-            //     'status' => 400, // Bad Request HTTP Status code
-            //     'message' => 'Missing required fields',
-            //     'fields' => $fieldsNotFound
-            // ];
         }
+
+        global $wpdb;
+
+        $table_name = $wpdb->prefix . 'pipi_datasources';
+
+        $wpdb->insert(
+            $table_name,
+            [
+                'datasource_name'        => $request['datasource_name'],
+                'datasource_url'         => $request['datasource_url'],
+                'datasource_credentials' => json_encode($request['datasource_credentials'])
+            ]
+        );
+
+        return [
+            'message' => 'Succesfully stored datasource'
+        ]
     }
 }
