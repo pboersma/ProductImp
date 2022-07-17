07a98f25d970e781ca92e2cd607e845c928a6ba8
Peter Boersma <peter@growingminds.nl>
2022-07-17 21:18:45 +0200

    Adjusted FrontEnd to include datasources

--- src/productimp/Visualization/Viewmaker.php
+++ src/productimp/Visualization/Viewmaker.php
@@ -15,9 +15,9 @@ class productimp_Visualization_Viewmaker {
     public function createView()
     {
         echo "<div id='app'></div>";
-        wp_enqueue_script('productimp_app-chunk', plugin_dir_url( __FILE__ ) . 'dist/js/chunk-vendors.27fc6bd3.js', [], '1.0', true);
-        wp_enqueue_script('productimp_app-vue', plugin_dir_url( __FILE__ ) . 'dist/js/app.27ae06da.js', [], '1.0', true);
-        wp_enqueue_style('productimp_app-styling', plugin_dir_url( __FILE__ ) . 'dist/css/app.b1d1ea26.css', [], '1.0', 'all');
+        wp_enqueue_script('productimp_app-chunk', plugin_dir_url( __FILE__ ) . 'dist/js/chunk-vendors.231cac5d.js', [], '1.0', true);
+        wp_enqueue_script('productimp_app-vue', plugin_dir_url( __FILE__ ) . 'dist/js/app.584dfebd.js', [], '1.0', true);
+        wp_enqueue_style('productimp_app-styling', plugin_dir_url( __FILE__ ) . 'dist/css/app.7e179905.css', [], '1.0', 'all');
         wp_enqueue_style('productimp_font-awesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css', [], '1.0', 'all');
     }
 }
