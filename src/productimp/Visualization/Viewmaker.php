ea421137a9c693592e7de75907592217015fd276
Peter Boersma <peter@growingminds.nl>
2022-07-17 14:55:58 +0200

    Changed Chunk JS wp key

--- src/productimp/Visualization/Viewmaker.php
+++ src/productimp/Visualization/Viewmaker.php
@@ -15,7 +15,7 @@ class productimp_Visualization_Viewmaker {
     public function createView()
     {
         echo "<div id='productimp_app'></div>";
-        wp_enqueue_script('productimp_app-vue', plugin_dir_url( __FILE__ ) . 'dist/chunk-vendors.5566ca25.js', [], '1.0', true);
+        wp_enqueue_script('productimp_app-chunk', plugin_dir_url( __FILE__ ) . 'dist/chunk-vendors.5566ca25.js', [], '1.0', true);
         wp_enqueue_script('productimp_app-vue', plugin_dir_url( __FILE__ ) . 'dist/app.fbd742dd.js', [], '1.0', true);
         // wp_enqueue_style('productimp_app-styling', plugin_dir_url( __FILE__ ) . 'dist/App.css', [], '1.0', 'all');
         wp_enqueue_style('productimp_font-awesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css', [], '1.0', 'all');
