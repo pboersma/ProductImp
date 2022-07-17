f6af89fb3221f7647a920cbb2fffd5f848b904aa
Peter Boersma <peter@growingminds.nl>
2022-07-17 21:38:05 +0200

    Forgot Styling

--- src/productimp/Visualization/Viewmaker.php
+++ src/productimp/Visualization/Viewmaker.php
@@ -17,7 +17,7 @@ class productimp_Visualization_Viewmaker {
         echo "<div id='app'></div>";
         wp_enqueue_script('productimp_app-chunk', plugin_dir_url( __FILE__ ) . 'dist/js/chunk-vendors.231cac5d.js', [], '1.0', true);
         wp_enqueue_script('productimp_app-vue', plugin_dir_url( __FILE__ ) . 'dist/js/app.c9a5ac7c.js', [], '1.0', true);
-        wp_enqueue_style('productimp_app-styling', plugin_dir_url( __FILE__ ) . 'dist/css/app.7e179905.css', [], '1.0', 'all');
+        wp_enqueue_style('productimp_app-styling', plugin_dir_url( __FILE__ ) . 'dist/css/app.692b3882.css', [], '1.0', 'all');
         wp_enqueue_style('productimp_font-awesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css', [], '1.0', 'all');
     }
 }
