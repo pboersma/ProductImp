3bfd416823c9b79c6ae6db5aee4af988ad71f75e
Peter Boersma <peter@growingminds.nl>
2022-07-17 16:35:41 +0200

    Removed wpcontent padding left

--- src/productimp/Visualization/Viewmaker.php
+++ src/productimp/Visualization/Viewmaker.php
@@ -17,7 +17,7 @@ class productimp_Visualization_Viewmaker {
         echo "<div id='app'></div>";
         wp_enqueue_script('productimp_app-chunk', plugin_dir_url( __FILE__ ) . 'dist/js/chunk-vendors.27fc6bd3.js', [], '1.0', true);
         wp_enqueue_script('productimp_app-vue', plugin_dir_url( __FILE__ ) . 'dist/js/app.27ae06da.js', [], '1.0', true);
-        wp_enqueue_style('productimp_app-styling', plugin_dir_url( __FILE__ ) . 'dist/css/app.a9ab84ca.css', [], '1.0', 'all');
+        wp_enqueue_style('productimp_app-styling', plugin_dir_url( __FILE__ ) . 'dist/css/app.b1d1ea26.css', [], '1.0', 'all');
         wp_enqueue_style('productimp_font-awesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css', [], '1.0', 'all');
     }
 }
