620bf5ef735fe31d750accf53a31ac9aee53f3be
Peter Boersma <peter@growingminds.nl>
2022-07-17 18:26:40 +0200

    used wrong activate function, adjusted.

--- productimp.php
+++ productimp.php
@@ -8,5 +8,5 @@ require_once dirname(__FILE__) . '/src/productimp/Autoloader.php';
 productimp_Autoloader::register();
 
 $productimp = new productimp_ProductImporter();
-add_action('plugin_activated', array($productimp, 'setup'));
+register_activation_hook( __FILE__, array($productimp, 'setup'));
 add_action('wp_loaded', array($productimp, 'load'));
