158b5913d26dcf92c8b46815a8ebf077b8e0edbf
Peter Boersma <peter@growingminds.nl>
2022-07-18 17:02:27 +0200

    Adjusted Autolader

--- productimp.php
+++ productimp.php
@@ -5,7 +5,7 @@ Plugin Name: Product Imp - Woocommerce Product Importer.
 
 // Setup class autoloader
 require_once dirname(__FILE__) . '/classes/Autoloader.php';
-productimp_Autoloader::register();
+productimp_Autoloader::register(true);
 
 $productimp = new productimp_ProductImporter();
 register_activation_hook( __FILE__, array($productimp, 'setup'));
