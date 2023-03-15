b544cb64c62e2608a5a440dae856efe5994ce1cb
Peter Boersma <peter@growingminds.nl>
2023-03-15 16:12:44 +0100

    Moved OLD Code to bin

--- CONCEPT/Authorization/Gatekeeper.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace ProductImp\Authorization;
-
-class Gatekeeper {
-  public function getAuthentication()
-  {
-    return base64_encode('p.boersma@lycan-media.nl:fuVk nnhg iEfv 5vF3 UgON ATNB');
-  }
-}
