34c34325369994bd36e4f6f73cc4c143881015bb
Peter Boersma <peter@growingminds.nl>
2023-03-16 10:48:29 +0100

    Moved App to Vite

--- vue.config.js
+++
@@ -1,7 +0,0 @@
-const { defineConfig } = require('@vue/cli-service');
-
-module.exports = defineConfig({
-  transpileDependencies: true,
-  outputDir: 'productimp/Visualization/dist/',
-  filenameHashing: false,
-});
